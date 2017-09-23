974d6e64bd685cdb80882685bdddf3de4f92e663
bjm37 <bjm37@calvin.edu>
2017-09-23 15:33:04 -0400

    Initial Slate

--- Grand Circus - LabSix/Grand Circus - LabSix/Program.cs
+++ Grand Circus - LabSix/Grand Circus - LabSix/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Grand_Circus___LabSix
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Pig Latin translator! Please enter a Word for translation!");
+            string inputWord = Console.ReadLine().ToLower();
+            char[] WordsArray = inputWord.ToCharArray();
+            string firstLetter = char.ToString(WordsArray[0]);
         }
     }
 }
