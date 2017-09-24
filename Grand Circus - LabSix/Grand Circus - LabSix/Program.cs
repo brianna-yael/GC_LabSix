c4c237cb2aa988fcbcd82834bfcb6e86b954eebe
bjm37 <bjm37@calvin.edu>
2017-09-24 12:39:53 -0400

    20170924

--- Grand Circus - LabSix/Grand Circus - LabSix/Program.cs
+++ Grand Circus - LabSix/Grand Circus - LabSix/Program.cs
@@ -1,14 +1,40 @@
 ﻿using System;
+//using System.Collections.Generic;
+using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
 namespace Grand_Circus___LabSix
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string inputWord, wordEnding, wordBeginning = "jane";
             Console.WriteLine("Welcome to the Pig Latin translator! Please enter a Word for translation!");
-            string inputWord = Console.ReadLine().ToLower();
-            char[] WordsArray = inputWord.ToCharArray();
-            string firstLetter = char.ToString(WordsArray[0]);
+
+            inputWord = Console.ReadLine().ToLower();
+            char[] word = inputWord.ToCharArray();
+            char v = word[0];
+            IsVowel(v);
+
+            if(true)
+            {
+                Console.WriteLine("false");
+            }
+        }
+
+        public static bool IsVowel(char c)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            if (vowels.Contains(c))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
