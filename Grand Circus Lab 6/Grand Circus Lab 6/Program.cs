e1b64a36e0032e9d72a38e473f6628252221c1ea
bjm37 <bjm37@calvin.edu>
2017-09-26 09:24:27 -0400

    Shortened the code, and added comments.

--- Grand Circus Lab 6/Grand Circus Lab 6/Program.cs
+++ Grand Circus Lab 6/Grand Circus Lab 6/Program.cs
@@ -10,71 +10,62 @@ namespace labSix
     {
         static void Main(string[] args)
         {
-            bool run = true;
-            while (run == true)
+            bool doAgain = true;
+            while (doAgain == true)
             {
                 Console.WriteLine("Welcome to the Pig Latin translator! Please enter a word for translation!");
                 string inputWord = Console.ReadLine().ToLower();
 
-
-                char[] letters = inputWord.ToCharArray();
-                if (IsVowel(letters[0]))
+                char[] letters = inputWord.ToCharArray(); //convert word to a character array
+                if (IsVowel(letters[0])) //run the isvowel method to determine whether the first letter in the array is a vowel
                 {
                     Console.WriteLine(inputWord + "-way");
                 }
                 else
                 {
-                    foreach (char l in letters)
+                    foreach (char l in letters) //loop through letter in the array
                     {
-                        bool b = IsVowel(l);
-                        if (b == true)
+                        if (IsVowel(l))
                         {
-                            int v = inputWord.IndexOf(l);
-                            string vowelEnd = inputWord.Substring(0, v);
-                            string vowelBegin = inputWord.Substring(v);
+                            int v = inputWord.IndexOf(l); //return the index of that vowel
+                            string vowelEnd = inputWord.Substring(0, v); // create a string that takes all the letters to the vowel
+                            string vowelBegin = inputWord.Substring(v); //create a string that takes all the letters from the vowel to the end of the word
                             Console.WriteLine(vowelBegin + "-" + vowelEnd + "ay");
-                            break;
+                            break; //so that it doesn't run the preceding four lines of code for every vowel in the array
                         }
                     }
                 }
-                run = Continue();
+                doAgain = Again();
             }
         }
 
         public static bool IsVowel(char c)
         {
             char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-            if (vowels.Contains(c))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return (vowels.Contains(c)); //returns true if vowel, false if not
         }
 
-        public static bool Continue()
+        public static bool Again()
         {
-            Console.WriteLine("Do you wish to Continue? y/n");
-            string input = Console.ReadLine();
-            input = input.ToLower();
-            bool goOn;
-            if (input == "y")
+            Console.WriteLine("Enter another word? Y/N");
+            string response = Console.ReadLine();
+            response = response.ToUpper();
+            bool anotherWord;
+            if (response == "Y")
             {
-                goOn = true;
+                anotherWord = true;
             }
-            else if (input == "n")
+            else if (response == "N")
             {
-                goOn = false;
+                anotherWord = false;
             }
             else
             {
                 Console.WriteLine("I don't understand that, let's try again");
-                goOn = Continue();
+                anotherWord = Again();
             }
 
-            return goOn;
+            return anotherWord;
         }
     }
 }
